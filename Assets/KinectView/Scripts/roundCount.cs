aed90359363fe157d3a669ff2ca255f44da45f16
hual0827 <hual0827@colorado.edu>
2022-04-27 15:23:50 -0600

    SFX
    Flipper disengage, game over.

--- Assets/KinectView/Scripts/roundCount.cs
+++ Assets/KinectView/Scripts/roundCount.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class roundCount : MonoBehaviour
 {
     public BodySourceView b;
+    public AudioSource gameOver;
 
     void OnTriggerEnter(Collider other)
     {
+        if(b.round == 3)
+        {
+            gameOver.Play();
+        }
         b.round++;
         b.fallen = true;
     }
